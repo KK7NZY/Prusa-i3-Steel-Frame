08455ef82484c892f1cedb505f1aeedfa6e13ee1
John Santiago Jr <jdsantiagojr@users.noreply.github.com>
2017-02-08 22:11:08 -0500

    M303 E0 S200 C8
    M301 P18.13 I1.07 D76.84

--- firmware/Configuration.h
+++ firmware/Configuration.h
@@ -157,9 +157,9 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+    #define  DEFAULT_Kp 18.13
+    #define  DEFAULT_Ki 1.07
+    #define  DEFAULT_Kd 76.84
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
